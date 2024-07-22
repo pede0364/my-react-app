03468b2b1a919b6f58f0bbc81385853fffb5ffaf
Aimee Pederson <aimee.pederson@gmail.com>
2024-07-22 16:11:43 -0500

    create boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Hero />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/Components/Hero.tsx
@@ -0,0 +1,11 @@
+import "../App.css";
+
+const Hero = () => {
+  return (
+    <div className="full-width bg-rose">
+      <h2>Another Hero</h2>
+    </div>
+  );
+};
+
+export default Hero;
